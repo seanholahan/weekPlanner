4d275b0940ba22788984c6204ad00c3ab4beba38
seanholahan <holahan.s@husky.neu.edu>
2015-11-16 17:34:21 -0500

    Update SetEvent.java

--- SetEvent.java
+++ SetEvent.java
@@ -1,1 +1,16 @@
 
+package weekPlanner;
+
+import java.util.Date;
+
+public class SetEvent extends Event{
+	boolean canMiss;
+
+	public SetEvent(String name, Date startTime, int duration, boolean canMiss) {
+		super(name, startTime, duration);
+		// TODO Auto-generated constructor stub
+		this.canMiss = canMiss;
+	}
+
+}
+
