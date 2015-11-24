4d1a8e2d05222aeac7b45400fd5f3894b9f7fae7
seanholahan <holahan.s@husky.neu.edu>
2015-11-23 19:49:03 -0500

    Update Event.java

--- src/weekPlanner/Event.java
+++ src/weekPlanner/Event.java
@@ -1,14 +1,20 @@
 package weekPlanner;
 
 import java.util.Date;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+import java.time.LocalDateTime;
 
 public abstract class Event {
     String name;
-    Date startTime; // flexible events won't know their start time at the time of initialization
-    int duration;
+    LocalDateTime startTime; // flexible events won't know their start time at the time of initialization :  THINK OF IT AS 
+    //WHEN YOU WANT TO START EVEN WORKING ON THE PROJECT OR THINKING ABOUT IT
+    Duration duration;
     private int importance; // 1-10
 
-    public Event(String name, Date startTime, int duration, int importance) {
+    public Event(String name, LocalDateTime startTime, Duration duration, int importance) {
         this.name = name;
         this.startTime = startTime;
         this.duration = duration;
@@ -21,12 +27,12 @@ public abstract class Event {
         return this.importance;
     }
     
-    public Date getStartTime() {
+    public LocalDateTime getStartTime() {
         return this.startTime;
     }
     
-    //public int getEndTime() {
-        // need a method to get the end time
-    //}
+    public LocalDateTime getEndTime() {
+        return startTime.plusMinutes(duration.toMinutes());
+    }
 
 }
