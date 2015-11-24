d92660fd572a2a01ea4ba553cf513f13f2e0a63c
seanholahan <holahan.s@husky.neu.edu>
2015-11-23 19:49:25 -0500

    Update FlexibleEvent.java

--- src/weekPlanner/FlexibleEvent.java
+++ src/weekPlanner/FlexibleEvent.java
@@ -1,14 +1,16 @@
 package weekPlanner;
-
+import java.time.Duration;
 import java.util.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class FlexibleEvent extends Event {
-    private Date dueDate;
+    private LocalDateTime dueDate;
     private boolean splitable; // later
     private int minChunkSize; // later
 
-    public FlexibleEvent(String name, Date startTime, int duration,
-            int importance, Date dueDate, boolean splitable, int minChunkSize)
+    public FlexibleEvent(String name, LocalDateTime startTime, Duration duration,
+            int importance, LocalDateTime dueDate, boolean splitable, int minChunkSize)
     {
         super(name, startTime, duration, importance);
         this.dueDate = dueDate;
